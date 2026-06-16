c20e5db3f9e989ee4b1d18d6be218a3a58c873e0
Eduardo Antonelli <eduardoantonellipersonal@gmail.com>
2026-06-16 08:56:59 -0300

    feat: adiciona testes de login

---
+++ tests/test_login.py
@@ -0,0 +1,56 @@
+import requests
+import random
+
+BASE_URL = "https://serverest.dev/login"
+
+def test_login_com_sucesso():
+    # 1. Gera um e-mail único e cadastra o usuário direto
+    email = f"qa_{random.randint(1000, 9999)}@teste.com"
+    requests.post("https://serverest.dev/usuarios", json={
+        "nome": "QA Admin", "email": email, "password": "123", "administrador": "true"
+    })
+    
+    # 2. Faz o login e valida
+    resposta = requests.post(BASE_URL, json={"email": email, "password": "123"})
+    dados = resposta.json()
+    
+    assert resposta.status_code == 200
+    assert dados["message"] == "Login realizado com sucesso"
+    assert "authorization" in dados
+    
+def test_login_com_senha_errada():
+    payload = {
+        "email": "fulano@qa.com.br",
+        "password": "senha_totalmente_errada_123"
+    }
+    
+    resposta = requests.post(BASE_URL, json=payload)
+    dados = resposta.json()
+    
+    assert resposta.status_code == 401
+    assert dados["message"] == "Email e/ou senha inválidos"
+
+def test_login_com_email_inexistente():
+    payload = {
+        "email": "esse_email_nao_existe_mesmo_2026@qa.com",
+        "password": "teste"
+    }
+    
+    resposta = requests.post(BASE_URL, json=payload)
+    dados = resposta.json()
+    
+    assert resposta.status_code == 401
+    assert dados["message"] == "Email e/ou senha inválidos"
+
+def test_login_com_campos_vazios():
+    payload = {
+        "email": "",
+        "password": ""
+    }
+    
+    resposta = requests.post(BASE_URL, json=payload)
+    dados = resposta.json()
+    
+    assert resposta.status_code == 400
+    assert dados["email"] == "email não pode ficar em branco"
+    assert dados["password"] == "password não pode ficar em branco"
